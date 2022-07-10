7c8d5d83e7d0b856cd603a9b7491e3be75376168
vivekdhir77 <vivekdhir77@gmail.com>
2022-07-10 19:45:10 +0530

    Predicting sentiments and emotions on preprocessed data

--- Predicting/emotion-classifier.py
+++ Predicting/emotion-classifier.py
@@ -8,8 +8,8 @@ import pandas as pd
 tokenizer = RobertaTokenizerFast.from_pretrained("arpanghoshal/EmoRoBERTa")
 model = TFRobertaForSequenceClassification.from_pretrained("arpanghoshal/EmoRoBERTa")
 emotion = pipeline('sentiment-analysis', model='arpanghoshal/EmoRoBERTa')
-
-data= pd.read_csv("cleaned_data.csv")
+CLEANED_DATA_FILE = "../Preprocessing/pre-processed-data.csv"
+data= pd.read_csv(CLEANED_DATA_FILE)
 emotion_data = []
 i = 0
 for text in data.text:
@@ -18,7 +18,7 @@ for text in data.text:
         emotion_data.append([text,emotion_labels[0]["label"], emotion_labels[0]["score"]])
         i+=1
         print(f"{i} done")
-        print(f"{emotion_labels[0]['label']} {emotion_labels[0]['score']} = {text}")
+        # print(f"{emotion_labels[0]['label']} {emotion_labels[0]['score']} = {text}")
     except:
         emotion_data.append([text,"error", "error"])
 
--- Predicting/sentiment-analyser.py
+++ Predicting/sentiment-analyser.py
@@ -1,5 +1,6 @@
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import pandas as pd
+import numpy as np
 def sentiment_scores(sentence):
  
     # Create a SentimentIntensityAnalyzer object.
@@ -7,7 +8,7 @@ def sentiment_scores(sentence):
  
     sentiment_dict = sid_obj.polarity_scores(sentence)
      
-    # print("Overall sentiment dictionary is : ", sentiment_dict)
+    # print("Overall sentiment: ", sentiment_dict)
     # print("sentence was rated as ", sentiment_dict['neg']*100, "% Negative")
     # print("sentence was rated as ", sentiment_dict['neu']*100, "% Neutral")
     # print("sentence was rated as ", sentiment_dict['pos']*100, "% Positive")
@@ -21,15 +22,22 @@ def sentiment_scores(sentence):
     return [sentiment_dict['neg']*100, sentiment_dict['neu']*100, sentiment_dict['pos']*100, final_result]
 
 if __name__ == "__main__" :
-    CLEANED_DATA_FILE = "YOUR FILE PATH HERE"
-    RESULTANT_FILE = "YOUR FILE PATH HERE"
+    CLEANED_DATA_FILE = "../Preprocessing/pre-processed-data.csv"
+    RESULTANT_FILE = "pre-processed-data_sentiments.csv"
     tweets = pd.read_csv(CLEANED_DATA_FILE)
     # tweets = tweets.head(5)
     result = []
-    # print(tweets['Text'])
-    for tweet in tweets['Text']:
-        sentiment = sentiment_scores(tweet)
-        result.append(sentiment)
+    # print(tweets['text'][0])
+    i = 0
+    for tweet in tweets['text']:
+        try:
+            # print(tweet)
+            print(i)
+            sentiment = sentiment_scores(tweet)
+            result.append(sentiment)
+            i = i+1
+        except:
+            result.append([np.nan,np.nan,np.nan, np.nan])
     # print(result)
     tweets[['neg', 'neu','pos', 'polarity']] = result
     tweets.to_csv(RESULTANT_FILE, index=False)
